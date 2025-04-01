5720f7e5ff49279a839ea89a2b414b576a11ee09
Newguidance1 <newguidance4141@gmail.com>
2025-04-01 13:07:41 -0700

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import logo from '../assets/mylogo/mylogo.svg';
+import logo from '../assets/mylogo/mylogo-1.svg';
 import { RiCloseLine, RiMenu3Line } from '@remixicon/react';
 import { Link } from 'react-scroll';
 
